c7468e36c3d2a5faccad8e3a5139d16baaa7ce71
metamn <bartus.csongor@gmail.com>
2014-06-24 11:26:49 +0300

    Starting a new slide

---
+++ _site/assets/scripts/jquery.init.js
@@ -0,0 +1,33 @@
+// Subparts of this script can be found in atoms/, molecules/ etc.
+
+$('document').ready(function() {
+  
+  // Don't cache submodules
+  $.ajaxSetup({
+    cache: false
+  });
+  
+  
+  // Slideshow
+ 
+  // Show slide navigator on the Beauty slide
+  $('#beauty-slideshow').bind('slideshowItemClicked', function(event, item) { 
+    if (item == 2) {
+      $('#quotes .navbar').addClass('active'); 
+      $('#play').addClass('active');
+    }
+  });
+  
+  $.getScript('assets/scripts/molecules/slideshow.js');
+  
+  
+  // Scrolling down to a slide
+  $.getScript('assets/scripts/molecules/section-navbar.js');
+  
+  
+  // Helpers
+  // ---------------------------------------
+  
+  // Scrolling to a div
+  $.getScript('assets/scripts/atoms/scrollTo.js');
+});
